d287035240fa293fb3843b21ab48b879bb0b70a7
夏华林 <hua_lin@live.cn>
2018-01-08 15:42:20 +0800

    push

--- swn73_vimrc_install.sh
+++ swn73_vimrc_install.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+set -e
 
-mkdir -p ~/.vim/bundle/
+cd ~/.vim_swn73
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-cp .vimrc ~/.
+echo 'set runtimepath+=~/.vim_runtime
+source ~/.vim_runtime/vimrcs/basic.vim
+source ~/.vim_runtime/vimrcs/filetypes.vim
+source ~/.vim_runtime/vimrcs/plugins_config.vim
+source ~/.vim_runtime/vimrcs/extended.vim
+try
+source ~/.vim_runtime/my_configs.vim
+catch
+endtry' > ~/.vimrc
